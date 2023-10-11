91eb2ccc07a41cefc0c903ace137a626f2849939
Victor <victrwng@gmail.com>
2023-10-11 10:41:30 -0700

    Iniital startup

---
+++ main.js
@@ -0,0 +1,15 @@
+const path = require("path");
+/*
+ * Project: Milestone 1
+ * File Name: main.js
+ * Description:
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const IOhandler = require("./IOhandler");
+const zipFilePath = path.join(__dirname, "myfile.zip");
+const pathUnzipped = path.join(__dirname, "unzipped");
+const pathProcessed = path.join(__dirname, "grayscaled");
